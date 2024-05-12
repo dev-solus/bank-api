7bca17ebf81ec5c061e2a0dc7733f64532098052
mohamed-mourabit <mohamed.mourabit@digitransform.co>
2024-05-12 03:54:18 +0100

    chore: Enable Web MVC in AppApplication

--- src/main/java/com/bank/app/AppApplication.java
+++ src/main/java/com/bank/app/AppApplication.java
@@ -2,20 +2,13 @@ package com.bank.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-// @OpenAPIDefinition(info = @Info(title = "Bank Documentation", version = "3.0", description = "Bank API"))
-// @SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@EnableWebMvc
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
