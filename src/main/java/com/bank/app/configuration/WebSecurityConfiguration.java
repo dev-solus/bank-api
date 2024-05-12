7db76da5262745da8a80ab5b9f16e12d3ec31d9e
mohamed-mourabit <mohamed.mourabit@digitransform.co>
2024-05-12 11:58:38 +0100

    chore: Update WebSecurityConfiguration to use Maven 3.9.6 and remove unused Swagger configuration file

--- src/main/java/com/bank/app/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/bank/app/configuration/WebSecurityConfiguration.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,39 +21,38 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class WebSecurityConfiguration {
 
-    // @Value("${springdoc.api-docs.path}")
-    // private String restApiDocPath;
-    // @Value("${springdoc.swagger-ui.path}")
-    // private String swaggerPath;
+    @Value("${springdoc.api-docs.path}")
+    private String restApiDocPath;
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerPath;
 
-    // @Autowired
-    // private Logger logger;
-    // // @Autowired
-    // private JwtTokenFilter jwtTokenFilter;
+    @Autowired
+    private Logger logger;
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
 
-    // public WebSecurityConfiguration(Logger logger, JwtTokenFilter jwtTokenFilter) {
-    //     super();
+    // public WebSecurityConfiguration(Logger logger, JwtTokenFilter jwtTokenFilter)
+    // {
+    // super();
 
-    //     this.logger = logger;
-    //     this.jwtTokenFilter = jwtTokenFilter;
+    // this.logger = logger;
 
-    //     // Inherit security context in async function calls
-    //     SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    // // Inherit security context in async function calls
+    // SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     // }
 
     // Set password encoding schema
-    // @Bean
-    // public BCryptPasswordEncoder passwordEncoder() {
-    //     return new BCryptPasswordEncoder();
-    // }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Enable CORS and disable CSRF
-        // http = http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        http = http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http = http.csrf(csrf -> csrf.disable());
 
         // Set session management to stateless
@@ -63,7 +60,7 @@ public class WebSecurityConfiguration {
 
         // Set unauthorized requests exception handler
         http = http.exceptionHandling(handling -> handling.authenticationEntryPoint((request, response, ex) -> {
-            // logger.error("Unauthorized request - {}", ex.getMessage());
+            logger.error("Unauthorized request - {}", ex.getMessage());
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
         }));
 
@@ -77,21 +74,21 @@ public class WebSecurityConfiguration {
                 .anyRequest().authenticated());
 
         // Add JWT token filter
-        // http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    // @Bean
-    // CorsConfigurationSource corsConfigurationSource() {
-    //     final var configuration = new CorsConfiguration();
-    //     configuration.setAllowedOrigins(Arrays.asList("*"));
-    //     configuration.setAllowedMethods(Arrays.asList("*"));
-    //     configuration.setAllowedHeaders(Arrays.asList("*"));
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
 
-    //     final var source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", configuration);
-    //     return source;
-    // }
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
