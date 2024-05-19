ec12a849cf192400e465aedcc53b0d4656e02ae1
mohamed-mourabit <mohamed.mourabit@digitransform.co>
2024-05-20 00:22:48 +0100

    chore: Update AppApplication.java to exclude WebMvcAutoConfiguration

--- src/main/java/com/bank/app/AppApplication.java
+++ src/main/java/com/bank/app/AppApplication.java
@@ -2,16 +2,28 @@ package com.bank.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerMapping;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
-@SpringBootApplication
-public class AppApplication {
+// @SpringBootApplication
+// public class AppApplication {
 
-	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.load();
-        // System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        // System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
-        // System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-		SpringApplication.run(AppApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		// Dotenv dotenv = Dotenv.load();
+//         // System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+//         // System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
+//         // System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+// 		SpringApplication.run(AppApplication.class, args);
+// 	}
+// }
+
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
 }
---
+++ src/main/java/com/bank/app/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package com.bank.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+        
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
